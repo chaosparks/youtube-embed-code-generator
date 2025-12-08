79691a3ad188c9af4600d40003a62cf94666d24c
ChaoSparks Notes <chaosparks123@gmail.com>
2025-12-08 21:04:43 +0800

    + fix depoly error

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Youtube, Link, Image as ImageIcon } from 'lucide-react';
+import { Youtube, Link} from 'lucide-react';
 
 interface InputSectionProps {
   videoUrl: string;
